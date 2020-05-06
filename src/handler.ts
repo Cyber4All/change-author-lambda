89d9ae4b4c46da2e19aa57cc4f46cdae6e715538
Emily Vogel <eav990@gmail.com>
2020-05-06 15:29:35 -0400

    Add copyFiles function

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,18 @@
 import { MongoDB } from "./drivers/database/mongodb";
 
-const { Client } = require('@elastic/elasticsearch')
+const { Client } = require('@elastic/elasticsearch');
 const AWS = require('aws-sdk');
 
 // Dotenv setup
-require('dotenv').config()
+require('dotenv').config();
+
+let client, s3;
 
 //@ts-ignore
 export const changeObjectAuthorHandler = async (event, context, callback) => {
+    client = setupElasticsearch();
+    s3 = setupAWS();
+
     // event.body gets the body of the request
     // body will be structured as such: {
     //      from:       ID of the person to transfer from
@@ -18,8 +23,6 @@ export const changeObjectAuthorHandler = async (event, context, callback) => {
     const toUserID = '';
     const objectIDs = [];
 
-    const client = setupElasticsearch();
-    const s3 = setupAWS();
     const db = await MongoDB.getInstance();
 
     let fromObjects = await db.getAuthorLearningObjects(fromUserID, objectIDs);
@@ -27,6 +30,11 @@ export const changeObjectAuthorHandler = async (event, context, callback) => {
     let oldAuthor = await db.getUserAccount(fromUserID);
     let newAuthorAccessID = await db.getFileAccessID(newAuthor.username);
     let oldAuthorAccessID = await db.getFileAccessID(oldAuthor.username);
+
+    fromObjects.map(async learningObject => {
+        const cuid = learningObject.cuid;
+        await copyFiles(null, cuid, oldAuthorAccessID, newAuthorAccessID);
+    });
 };
 
 /**
@@ -52,4 +60,32 @@ function setupAWS() {
     AWS.region = AWS_SDK_CONFIG.region;
     
     return new AWS.S3();
+}
+
+async function copyFiles(token, fromCuid, oldAuthorAccessID, newAuthorAccessID) {
+    const s3Options = { Bucket: process.env.BUCKET_NAME, Prefix: `${oldAuthorAccessID.fileAccessId}/${fromCuid}` };
+    if(token) {
+        s3Options['ContinuationToken'] = token;
+    }
+
+    let allKeys = [];
+    s3.listObjectsV2(s3Options, function(err, data) {
+        allKeys = allKeys.concat(data.Contents);
+    
+        if(data.IsTruncated)
+            copyFiles(data.NextContinuationToken, fromCuid, oldAuthorAccessID, newAuthorAccessID);
+        else {
+            allKeys.map(async key => {
+                if (!key.Key.includes(`${fromCuid}.zip`)) {
+                    console.log('OLD KEY: ', key.Key);
+                    console.log('NEW KEY: ', `${newAuthorAccessID.fileAccessId}${key.Key.replace(oldAuthorAccessID.fileAccessId, '')}`);
+                    await s3.copyObject({
+                        Bucket: process.env.BUCKET_NAME,
+                        CopySource: `${process.env.BUCKET_NAME}/${key.Key}`,  // old file Key
+                        Key: `${newAuthorAccessID.fileAccessId}${key.Key.replace(oldAuthorAccessID.fileAccessId, '')}`, // new file Key
+                    }).promise();
+                }
+            });
+        }
+    });
 }
